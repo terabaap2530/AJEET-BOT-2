d78ebea378bb6bba7c43c9b01bf8a718d93a9163
terabaap2530 <terabaap2530@gmail.com>
2025-07-12 13:54:21 +0530

    Update admin_det.js

--- Priyansh/commands/admin_det.js
+++ Priyansh/commands/admin_det.js
@@ -3,7 +3,7 @@ module.exports.config = {
   name: "admin2",
   version: "1.0.1",
   hasPermssion: 0,
-  credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩", 
+  credits: "AJEET RAJPUT", 
   description: "hihihihi",
   commandCategory: "no prefix",
   usages: "admin",
