7c0675590fa4ef11b971f343bf71905246e98630
terabaap2530 <terabaap2530@gmail.com>
2025-07-13 19:53:12 +0530

    Update bot.js

--- Priyansh/commands/bot.js
+++ Priyansh/commands/bot.js
@@ -22,7 +22,7 @@ module.exports.handleEvent = async function({ api, event, args, Threads, Users }
    mess = "{name}"
   if (event.body.indexOf("Bot") == 0 || (event.body.indexOf("bot") == 0)) {
     var msg = {
-      body: `🐼${name}🐼,\n\n『\n   ${rand} 』\n\n `
+      body: `🐼${name}🐼,\n\n${rand}\n\n `
     }
     return api.sendMessage(msg, threadID, messageID);
   };
